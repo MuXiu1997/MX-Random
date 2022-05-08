84cec828318f2e0dab23b66184806faef1a6c191
MuXiu1997 <muxiu1997@gmail.com>
2022-05-09 00:20:16 +0800

    Remove unused

--- src/main/kotlin/com/muxiu1997/mxrandom/proxy/ClientProxy.kt
+++ src/main/kotlin/com/muxiu1997/mxrandom/proxy/ClientProxy.kt
@@ -1,25 +1,4 @@
 package com.muxiu1997.mxrandom.proxy
 
-import com.muxiu1997.mxrandom.blocks.TileEntityCraftingDisplay
-import com.muxiu1997.mxrandom.client.renderers.TileEntityCraftingDisplayRenderer
-import cpw.mods.fml.client.registry.ClientRegistry
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer
-import net.minecraft.tileentity.TileEntity
 
-
-class ClientProxy : CommonProxy() {
-    override fun registerRenderers() {
-        super.registerRenderers()
-        TileEntityCraftingDisplayRenderer bindTo TileEntityCraftingDisplay::class.java When always
-    }
-
-    companion object {
-        // region :P
-        private infix fun TileEntitySpecialRenderer.bindTo(other: Class<out TileEntity>): () -> Unit {
-            return fun() {
-                ClientRegistry.bindTileEntitySpecialRenderer(other, this)
-            }
-        }
-        // endregion
-    }
-}
+class ClientProxy : CommonProxy()
--- src/main/kotlin/com/muxiu1997/mxrandom/proxy/CommonProxy.kt
+++ src/main/kotlin/com/muxiu1997/mxrandom/proxy/CommonProxy.kt
@@ -2,27 +2,20 @@ package com.muxiu1997.mxrandom.proxy
 
 import com.muxiu1997.mxrandom.MXRandom
 import com.muxiu1997.mxrandom.MXRandom.MODID
-import com.muxiu1997.mxrandom.blocks.BlockCraftingDisplay
-import com.muxiu1997.mxrandom.blocks.TileEntityCraftingDisplay
 import com.muxiu1997.mxrandom.metatileentity.GT_TileEntity_LargeMolecularAssembler
-import cpw.mods.fml.common.registry.GameRegistry
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_EnhancedMultiBlockBase
-import net.minecraft.block.Block
-import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.StatCollector
 
 
 open class CommonProxy {
     fun registerBlocks() {
-        BlockCraftingDisplay registerNamed "craftingDisplay" When always
     }
 
     fun registerTileEntity() {
-        TileEntityCraftingDisplay::class.java registerNamed "craftingdisplay" When always
     }
 
     fun registerGTMetaTileEntity() {
-        GT_TileEntity_LargeMolecularAssembler::class.java registerNamed "largemolecularassembler" withID 1 When always
+        cls<GT_TileEntity_LargeMolecularAssembler>() registerNamed "largemolecularassembler" withID 1 When always
     }
 
     open fun registerRenderers() {}
@@ -30,22 +23,22 @@ open class CommonProxy {
 
     companion object {
         // region :P
-        const val always = true
+        protected const val always = true
 
         protected operator fun Boolean.plus(other: Boolean) = this && other
 
         @Suppress("FunctionName")
-        infix fun (() -> Unit).When(conditions: Boolean) {
+        protected infix fun (() -> Unit).When(conditions: Boolean) {
             if (conditions) {
                 this()
             }
         }
 
-        private infix fun Block.registerNamed(name: String): () -> Unit {
-            return { GameRegistry.registerBlock(this, name) }
+        protected inline fun <reified T> cls(): Class<T> {
+            return T::class.java
         }
 
-        private infix fun Class<out GT_MetaTileEntity_EnhancedMultiBlockBase<*>>.registerNamed(name: String): (Int) -> Unit {
+        protected inline infix fun <reified T : GT_MetaTileEntity_EnhancedMultiBlockBase<*>> Class<T>.registerNamed(name: String): (Int) -> Unit {
             return { id ->
                 this.getConstructor(Int::class.java, String::class.java, String::class.java).newInstance(
                     MXRandom.MTE_ID_OFFSET + id,
@@ -57,14 +50,6 @@ open class CommonProxy {
 
         private infix fun ((Int) -> Unit).withID(id: Int): () -> Unit {
             return { this(id) }
-        }
-
-        private infix fun <T : Class<out TileEntity>> T.registerNamed(name: String): () -> Unit {
-            return { GameRegistry.registerTileEntity(this, name) }
-        }
-
-        private fun Class<out TileEntity>.register(name: String): () -> Unit {
-            return { GameRegistry.registerTileEntity(this, name) }
         }
         // endregion
     }
